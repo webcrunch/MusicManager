905be4ccb2134017dd9ab8cb6301417dc9216f45
jarl Lindquist <jarllinduist@gmail.com>
2022-09-30 10:26:28 +0200

    Adding a new level on the menu and adding save function and load function for the ItemStore.

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,58 @@
+import com.google.gson.annotations.JsonAdapter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-
+    @JsonAdapter(ItemListAdapter.class)
     public static List<Band> bandList = new ArrayList<>();
     public static void main(String[] args) {
-        Menu.mainMenu();
-
         if (!ItemFileHandler.fileExists("data.json")) {
+            // What will we do when we do not have a file yet?
             // Create som PetOwners and Pets
             //PetOwner anna = new PetOwner("Anna", "Andersson");
-            Band test = new Band("TestBand", "information about the band" ,2022, null);
-            Musician musician = new Musician("Stig", "alfatpehenere ", 1894);
-            test.addMember(musician);
-            ItemStore.save("data.json");
-            System.out.println("Saved some PetOwners and Pets to data.json...\n\n");
-            ItemStore.log();
+            //Band test = new Band("TestBand", "information about the band" ,2022, null);
+            //Musician musician = new Musician("Stig", "stensson ", 1894);
+            //band.addMember(musician);
+            //ItemStore.save("data.json");
+            //System.out.println("Saved some PetOwners and Pets to data.json...\n\n");
+            //ItemStore.log();
         }
-        // Otherwise we read our PetOwners and Pets from json via the ItemStore
-        else {
-            // Read all data to the store
-            ItemStore.load("data.json");
-            // Log the whole store
-            ItemStore.log();
-            // Check resurrecting - the pets property and the owner property should
-            // have completely resurrected objects...
-            System.out.println("\n\nResurrect check 1:");
-            //System.out.println(ItemStore.lists.petOwners.get(0).pets);
-            System.out.println("\n\nResurrect check 2:");
-            //System.out.println(ItemStore.lists.pets.get(2).owner);
-            System.out.println("Resurrect check 3");
-            //System.out.println(ItemStore.lists.toys.get(1).owner.owner);
+        else loadData();
+        while (true){
+            Menu.mainMenu();
         }
     }
 
+    private static void loadData(){
+        // Read all data to the store
+        ItemStore.load("data.json");
+        // Log the whole store
+        ItemStore.log();
+        // Check resurrecting - the pets property and the owner property should
+        // have completely resurrected objects...
+        System.out.println("\n\nResurrect check 1:");
+        //System.out.println(ItemStore.lists.petOwners.get(0).pets);
+        System.out.println("\n\nResurrect check 2:");
+        //System.out.println(ItemStore.lists.pets.get(2).owner);
+        System.out.println("Resurrect check 3");
+        //System.out.println(ItemStore.lists.toys.get(1).owner.owner);
+    }
 
+    private static void saveData(){
+        // Create som PetOwners and Pets
+        //PetOwner anna = new PetOwner("Anna", "Andersson");
+        Band band = new Band("TestBand", "information about the band" ,2022, null);
+        Musician musician = new Musician("Stig", "stensson ", 1894);
+        musician.addAlbum(new Album("Jazz hands", "an album with nice jazz", 2002));
+        musician.addCurrentBand(band);
+        band.addMember(musician);
+        ItemStore.save("data.json");
+        System.out.println("Saved some PetOwners and Pets to data.json...\n\n");
+        //ItemStore.log();
+    }
 
+    public static void getSaveData(){
+        saveData();
+    }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,60 +1,74 @@
 public class Menu {
 
-    public static void mainMenu(){ //mainMenu
-        switch(Input.menu("Vad vill du göra? ", "Lägg till", "Ta bort", "Visa")){
-            case "Lägg till":
-                handlingActions(Input.menu("Vad vill du Lägga till?", "Band", "Album", "Musician"), "Lägg till");
-                break;
-            case "ta bort":
-                handlingActions(Input.menu("Vad vill du ta bort?", "Band", "Album", "Musician"), "Ta bort");
-                break;
-            case "Visa":
-                handlingActions(Input.menu("Vad vill du Visa?" , "Band", "Album", "Musician"), "Visa");
-                break;
-            default:
-                break;
+    public static void mainMenu() { //mainMenu
+        switch (Input.menu("Vad vill du göra? ", "Lägg till", "Ta bort", "Visa", "spara")) {
+            case "Lägg till" ->
+                    handlingActions(Input.menu("Vad vill du Lägga till?", "Band", "Album", "Musician"), "Lägg till");
+            case "ta bort" ->
+                    handlingActions(Input.menu("Vad vill du ta bort?", "Band", "Album", "Musician"), "Ta bort");
+            case "Visa" -> handlingActions(Input.menu("Vad vill du Visa?", "Band", "Album", "Musician"), "Visa");
+            case "spara" -> {
+                Main.getSaveData();
+                mainMenu();
+            }
+            default -> mainMenu();
+        }
+    }
+    private static void elevatorFunction(String members, String action) {
+        switch (Input.menu("Vad vill du göra? ", "Start meny", "Upp ett steg")) {
+            case "Upp ett steg" -> handlingActions(members,action);
+            case "Start meny" -> mainMenu();
         }
     }
 
-    private static void handlingActions(String members, String action ){ // change name ?? menuOptions
-        switch (members){
+
+    private static void handlingActions(String members, String action) { // change name ?? menuOptions
+        switch (members) {
             case "Band":
-                if(action.equals("Visa")){
+                if (action.equals("Visa")) {
                     System.out.println("display on band");
+                    elevatorFunction(members,action);
                 } else if (action.equals("Lägg till")) {
                     // start with an easy coding to get something there.
                     String bandName = Input.string("Whats the bands name?");
                     String bandInfo = Input.string("Information about the band?");
                     Integer bandYear = Input.integer("What year did the band started?");
-                    Integer bandDisbanded = Input.integer("If the band has been disbanded. Otherwise just set it to -1") ;
+                    Integer bandDisbanded = Input.integer("If the band has been disbanded. Otherwise just set it to -1");
                     bandDisbanded = bandDisbanded < 0 ? null : bandDisbanded;
-                    Band bands = new Band(bandName,bandInfo,bandYear,bandDisbanded);
-                }else{
+                    Band bands = new Band(bandName, bandInfo, bandYear, bandDisbanded);
+                    elevatorFunction(members,action);
+                } else {
                     System.out.println("Otherwise remove band");
+                    elevatorFunction(members,action);
                 }
                 break;
             case "Album":
-                if(action.equals("Visa")){
+                if (action.equals("Visa")) {
                     System.out.println("display album");
                 } else if (action.equals("Lägg till")) {
                     String bandName = Input.string("Whats the Albums name?");
                     String bandInfo = Input.string("Information about the Album?");
                     int bandYear = Input.integer("What year did the Album publish?");
-                    Album albums = new Album(bandName,bandInfo,bandYear);
-                }else{
+                    Album albums = new Album(bandName, bandInfo, bandYear);
+                    elevatorFunction(members,action);
+                } else {
                     System.out.println("otherwise remove album");
+                    elevatorFunction(members,action);
                 }
                 break;
             case "Musician":
-                if(action.equals("Visa")){
+                if (action.equals("Visa")) {
                     System.out.println("Display Musician");
+                    elevatorFunction(members,action);
                 } else if (action.equals("Lägg till")) {
                     String name = Input.string("What is the name of the musician? ");
                     String info = Input.string("Information about the musician?");
                     Integer birthYear = Input.integer("What year is the musician born?");
-                    Musician musician = new Musician(name,info,birthYear);
-                }else{
+                    Musician musician = new Musician(name, info, birthYear);
+                    elevatorFunction(members,action);
+                } else {
                     System.out.println("otherwise remove Musician");
+                    elevatorFunction(members,action);
                 }
                 break;
         }
