7d9dde431bd5dd8ea92a647002e5b05cd2eb6853
Espen Lever <eko15ele@student.lu.se>
2022-10-05 13:32:27 +0200

    Added missing menu options to menus

--- src/Menu.java
+++ src/Menu.java
@@ -3,10 +3,10 @@ public class Menu {
     public static void mainMenu() { //mainMenu
         switch (Input.menu("Choose the  ", "Band", "Musician", "Album", "save")) {
             case "Band" ->
-                    handlingActions(Input.menu("What do you want to do in Band? ", "Add Band", "Remove Band", "Display Band(s)", "Add Member", "Remove Member"), "Band");
+                    handlingActions(Input.menu("What do you want to do in Band? ", "Add Band", "Remove Band", "Display Band(s)", "Add Member", "Remove Member", "Add Album", "Remove Album"), "Band");
             case "Musician" ->
-                    handlingActions(Input.menu("What do you want to do in Musician? ", "Add Musician", "Remove Musician", "Display Musician"), "Musician");
-            case "Album" -> handlingActions(Input.menu("What do you want to do in Albums? ", "Add Album", "Remove Album", "Display Album"), "Albums");
+                    handlingActions(Input.menu("What do you want to do in Musician? ", "Add Musician", "Remove Musician", "Display Musician", "Add Album", "Remove Album"), "Musician");
+            case "Album" -> handlingActions(Input.menu("What do you want to do in Albums? ", "Add Album", "Remove Album", "Display Album", "Add Contributor", "Remove Contributor"), "Albums");
             case "save" -> {
                 Main.getSaveData();
                 mainMenu();
