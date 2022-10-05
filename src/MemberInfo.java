49fbc9bc0f18d60253cc7c61f4b1b154b02ec467
webcrunch <jarllinduist@gmail.com>
2022-10-05 14:45:02 +0200

    add extends Item to the class

--- src/MemberInfo.java
+++ src/MemberInfo.java
@@ -1,19 +1,16 @@
-public class MemberInfo {
+public class MemberInfo  extends Item {
 
     private Musician musician;
     private Band band;
     private int yearJoined;
     private String instrument;
 
-    public MemberInfo (Musician musician,Band band, int yearJoined, String instrument) {
+    public MemberInfo(Musician musician,Band band, int yearJoined, String instrument) {
         this.musician = musician;
         this.band = band;
         this.yearJoined = yearJoined;
         this.instrument = instrument;
-
         ItemStore.add(this);
-
     }
-
 }
 
